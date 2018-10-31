34e1d5de34a30f448f6a7759714c234a9a200d8a
Sam <sam@islandearth.net>
2018-10-31 19:54:02 +0000

    Upload New File

---
+++ WrongIdException.java
@@ -0,0 +1,13 @@
+package net.islandearth.islandearth.utils;
+
+public class WrongIdException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4472556003462521095L;
+
+	public WrongIdException(String message) {
+		super(message);
+	}
+}
