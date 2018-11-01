fa5366a3294df055df0208ba72ad47bb65d4ee81
Sam <sam@islandearth.net>
2018-11-01 18:42:38 +0000

    Upload New File

---
+++ SchematicNotLoadedException.java
@@ -0,0 +1,13 @@
+package net.islandearth.islandearth.utils;
+
+public class SchematicNotLoadedException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8805283716076251297L;
+	
+	public SchematicNotLoadedException(String message) {
+		super(message);
+	}
+}
