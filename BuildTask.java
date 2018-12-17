2f0a66d3decd7adc2cf1b570ed0e40af461e9047
Sam <sam@islandearth.net>
2018-12-17 20:21:08 +0000

    Update BuildTask.java

--- BuildTask.java
+++ BuildTask.java
@@ -1,51 +1,52 @@
-import java.util.HashMap;
-import java.util.List;
-
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.entity.Player;
-
-import lombok.Getter;
-
 /**
  * @author SamB440
  */
-public class BuildTask implements Runnable {
+@RequiredArgsConstructor
+public class BuildTask {
 	
-	private JavaPlugin plugin;
-	@Getter private Map<Player, List<Location>> cache = new HashMap<>();
+	@NonNull
+	private MyPlugin plugin;
 	
-	public BuildTask(JavaPlugin plugin)
-	{
-		this.plugin = plugin;
-	}
-
-	@Override
-	public void run() {
-		
-		for(Player player : Bukkit.getOnlinePlayers())
-		{
-			if(plugin.getPlayerManagement().isBuilding(player.getUniqueId()))
-			{
-				List<Location> locations = plugin.getPlayerManagement().getBuilding(player.getUniqueId()).pasteSchematic(player.getTargetBlock(null, 7).getLocation().add(0, 1, 0), player, Options.PREVIEW);
-				Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, () -> {
-					if(cache.containsKey(player))
-					{
-						if(!cache.get(player).equals(locations))
-						{
-							int current = 0;
-							for(Location location : cache.get(player))
-							{
-								if(current == locations.size()) break;
-								if(location.distance(locations.get(current)) >= 1) player.sendBlockChange(location, location.getBlock().getBlockData());
-								current++;
+	@NonNull
+	private Player player;
+	
+	private Scheduler scheduler;
+	
+	@Getter 
+	private Map<Player, List<Location>> cache = new HashMap<>();
+	
+	@SuppressWarnings("deprecation")
+	public BuildTask start() {
+	    PlayerManagement pm = plugin.getPlayerManagement();
+		scheduler = new Scheduler();
+		scheduler.setTask(
+		Bukkit.getScheduler().scheduleAsyncRepeatingTask(plugin, () -> {
+			if (pm.isBuilding(player.getUniqueId())) {
+				Schematic schematic = civ.getBuilding();
+				try {
+					List<Location> locations = schematic.pasteSchematic(player.getTargetBlock(null, 10).getLocation().add(0, 1, 0), player, Options.PREVIEW);
+					Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, () -> {
+						if (cache.containsKey(player)) {
+							if (!cache.get(player).equals(locations)) {
+								int current = 0;
+								for (Location location : cache.get(player)) {
+									if (current == locations.size()) break;
+									if (location.distance(locations.get(current)) >= 1) player.sendBlockChange(location, location.getBlock().getBlockData());
+									current++;
+								}
+								cache.remove(player);
 							}
-							cache.remove(player);
 						}
-					}
-				if(!cache.containsKey(player)) cache.put(player, locations);
-				}, 2L);
+					if(!cache.containsKey(player)) cache.put(player, locations);
+					}, 2L);
+				} catch (SchematicNotLoadedException e) {
+					e.printStackTrace();
+				}
+			} else if (!pm.isBuilding(player.getUniqueId())) {
+				scheduler.cancel();
 			}
-		}
+		}, 5L, 1L));
+		
+		return this;
 	}
 }
